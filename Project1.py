09295d72def96255e1c240ad169eec51c30acb1e
shabbir-hossan <afrankhanshabbir@gmail.com>
2024-08-02 15:45:44 +0600

    added

---
+++ Project1.py
@@ -0,0 +1,11 @@
+name = 'Shabbir'
+age = 22
+price = 25.6
+
+print(name)
+print(type(age))
+print(price)
+
+print('shabbir Hossan \n 01631843152')
+print('shabbir Hossan \t 01631843152')
+print("\"shabbir Hossan\"")
