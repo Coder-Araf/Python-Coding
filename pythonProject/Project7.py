f44de433b2edf178020fbbc1e705ded541c2b19f
shabbir-hossan <afrankhanshabbir@gmail.com>
2024-08-04 22:03:29 +0600

    added

---
+++ pythonProject/Project7.py
@@ -0,0 +1,18 @@
+name = "Shabbir"
+age = 22
+bol1 = True
+flt = 25.3
+
+print(type(name))
+print(type(age))
+print(type(bol1))
+print(type(flt))
+
+#Oparator  ###Comment's
+a=1000
+b=500
+sum = a+b
+dif = a-b
+
+print(sum)
+print(dif)
---
+++ pythonProject/Project8.py
@@ -0,0 +1,28 @@
+#Arithmetic Operators
+
+a = 7
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+#Relational Operators
+
+x=50
+y=20
+
+print(x==y) #False
+print(x!=y) #True
+print(x>=y) #True
+print(x>y) #True
+
+#Assignment Operators
+num = 10
+num **=10
+print(num)
+
+#Logical Operators
